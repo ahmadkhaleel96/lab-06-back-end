be16a223fb74a1b7841992180d54e12d128acb21
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2019-12-22 16:21:19 +0200

    added the location data and created the server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const server = express();
@@ -10,3 +10,36 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 
 server.listen(PORT, () => console.log('Listening to PORT 5500'));
+
+server.get('/', (request, response) => {
+	response.status(200).send('YOU ARE MEAN!!!');
+});
+
+function Location(city, locationData){
+    this.formatted_query = locationData[0].display_name;
+    this.latitude = locationData[0].lat;
+    this.longitude = locationData[0].lon;
+    this.search_query = city;
+}
+server.get('/location', (request, response) => {
+    // Read the city from the user (request)
+    // find the city in geo.json
+    
+    const locationData = require('./data/geo.json');
+    let location = new Location("lynwood", locationData);
+    response.status(200).send(location);
+});
+
+// function Weather(city, weatherData){
+
+// }
+
+
+server.use('*', (request, response) => {
+	response.status(404).send('ALL I HAVE IS NEGATIVE THOUGHTS!!!');
+});
+server.use((error, request, response) => {
+	response.send(500).send(error);
+});
+
+
